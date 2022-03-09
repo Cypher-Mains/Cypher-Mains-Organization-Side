9eb182c20bb30ff07eba81726c4c40ae0220c78d
Sarath Adhithya <91727830+SarathAdhi@users.noreply.github.com>
2022-03-09 09:58:13 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,68 @@
+var qrcode = new QRCode("qrcode");
+var globalToken;
+
+function getRandomString(length) {
+    var randomChars = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz';
+    var result = '';
+    for (var i = 0; i < length; i++) {
+        result += randomChars.charAt(Math.floor(Math.random() * randomChars.length));
+    }
+    return result;
+}
+
+
+function qrCodeGenerator() {
+    var secretCode = getRandomString(32)
+    var link = `otpauth://totp/SarathTest?issuer=GithubTest&secret=${secretCode}&algorithm=SHA1&digits=6&period=30`;
+    console.log(link)
+
+    var qr = qrcode.makeCode(link);
+    return link;
+}
+
+function extractDetails(link) {
+    var splitQR = link.split('&');
+    var len = splitQR.length;
+
+    var result = [];
+    var details = [];
+
+    for (var i = 0; i < len; i++) {
+        result[i] = splitQR[i].split('=');
+        details[i] = result[i][1];
+    }
+    return details;
+}
+
+function generateOTP(link) {
+
+    var details = extractDetails(link);
+    let totp = new OTPAuth.TOTP({
+        issuer: details[0],
+        label: details[0],
+        algorithm: details[2],
+        digits: details[3],
+        period: details[4],
+        secret: details[1],
+    });
+
+    let token = totp.generate();
+    globalToken = token;
+    console.log(token)
+
+}
+
+function changeOTP() {
+    var link = qrCodeGenerator()
+    setInterval(() => generateOTP(link), 1000)
+}
+
+function checkToken() {
+    var getToken = document.getElementById('key').value;
+    if (getToken === globalToken)
+        alert("correct");
+    else
+        alert("Wrong");
+}
+
+changeOTP();
